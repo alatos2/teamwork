790f581ed34dafd06a32a56ca73d3e1309740ba0
alatos2 <alabitosin60@gmail.com>
2019-11-21 13:30:31 +0100

    code refactoring

--- server/controller/articles.js
+++ server/controller/articles.js
@@ -14,14 +14,13 @@ const createArticle = (req, res) => {
   const {
     title, article,
   } = req.body;
-  const { id } = req.decode;
   const result = validations.validateArticle(req.body);
   if (result.error) {
     const errorMsg = result.error.details[0].message;
     Responses.setError(400, errorMsg.replace(/[^a-zA-Z ]/g, ''));
     return Responses.send(res);
   }
-  const articleValues = [id, title, article, moment().format()];
+  const articleValues = [req.decode.id, title, article, moment().format()];
   ArticleModel.create(articleValues, res);
 };
 
@@ -92,7 +91,6 @@ const createComment = (req, res) => {
   const {
     comment,
   } = req.body;
-
   const result = validations.validateComment(req.body);
   if (result.error) {
     const errorMsg = result.error.details[0].message;
